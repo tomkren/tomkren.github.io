21b5622e6454286c4c2cf04ca0d228bd8ac4dbe1
Tom Kren <tomkren@gmail.com>
2013-12-07 00:54:54 +0100

    minor - par uprav v chystanem porodnim skriptu

--- kit/_tenkrat_poprve/porod.sh
+++ kit/_tenkrat_poprve/porod.sh
@@ -3,7 +3,10 @@ echo TODO : BACHA JE TO rozdelany
 echo
 echo Rodim server ...
 echo
-ln -s /home/domeny/tomkren.cz/web/subdomeny/git/ /git-repos
+ln -s /home/domeny/tomkren.cz/web/subdomeny/ /subdomeny
+ln -s /subdomeny/git/ /git-repos
+ln -s /subdomeny/wiki/ /wiki
+ln -s /subdomeny/blog/ /blog
 cd /git-repos
 git clone https://github.com/tomkren/tomkren/tomkren.github.io.git
 git clone https://github.com/tomkren/kutil.git
